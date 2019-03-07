f324859ad93a4cb166defb66597ec0c599efbcd3
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-03-07 12:29:08 -0500

    created reducers folder

---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,22 @@
+import uuid from "uuid";
+
+const initialState = {
+  items: [
+    {
+      id: uuid(),
+      name: "Eggs"
+    },
+    {
+      id: uuid(),
+      name: "Milk"
+    },
+    {
+      id: uuid(),
+      name: "Steak"
+    },
+    {
+      id: uuid(),
+      name: "Water"
+    }
+  ]
+};
